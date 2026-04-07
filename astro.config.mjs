38a83833e323c8fec3e5319add96779e1e1d34c1
Daniel Vlad <hello@ship.it>
2026-04-07 12:52:25 +0000

    fix: set base path for GitHub Pages deployment (dvlad-devops-lp)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/dvlad-devops-lp',
   integrations: [tailwind()],
   preview: {
     allowedHosts: true
